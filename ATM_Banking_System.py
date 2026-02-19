092bbc3e3e3df46116d7d572bc13dbb399274c2e
Aryan Bhardwaj <baaryan428@gmail.com>
2026-02-19 20:50:39 +0530

    Add files via upload

---
+++ ATM_Banking_System.py
@@ -0,0 +1,13 @@
+# Creating an ATM Banking System
+
+def Currentsaving():
+    amount = int(input("Enter the credit amount"))
+    print("Saving amount is ",amount)
+    def withdraw():
+        withdrawl= int(input("Enter the debetit amount:"))
+        print("Balance is",(amount-withdrawl))
+    
+    withdraw()
+    
+Currentsaving()
+
