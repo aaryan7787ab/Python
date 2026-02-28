0f32c15890eb20dff3364139ad5b5d5a47bb0ea4
Aryan Bhardwaj <baaryan428@gmail.com>
2026-02-28 20:31:15 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,14 @@
+#Create a star pattern 
+n = int(input("Enter the value of N :"))
+j = 1
+space= ""
+if(n <= 0):
+    print("Invalid Input!")
+else:
+    for i in range(j,n+1):
+        print(j*"*")
+        j+=1
+    j = n-1
+    for b in range(n-1,0,-1):
+            print("*"*b)
+            j-=1
